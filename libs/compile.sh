9d6b7bb9deeefb375d4af79ebe56b232c7542055
jiacai2050 <dev@liujiacai.net>
2024-04-03 22:23:41 +0800

    debug

--- libs/compile.sh
+++ libs/compile.sh
@@ -4,4 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 set -ex
 export CC="zig cc"
-(cd "${SCRIPT_DIR}/jemalloc" && ./autogen.sh && ./configure --disable-cxx --with-jemalloc-prefix="je_" && make)
+
+(cd "${SCRIPT_DIR}/jemalloc" && ./autogen.sh && ./configure --disable-cxx --with-jemalloc-prefix="je_" && make -j8)
+
+cat "${SCRIPT_DIR}/jemalloc/include/jemalloc/jemalloc.h"
