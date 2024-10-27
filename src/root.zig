1b893cdfccee2c1f4cc76158561b1a0ef54ef622
jiacai2050 <dev@liujiacai.net>
2024-10-27 19:25:49 +0800

    add stats print test

--- src/root.zig
+++ src/root.zig
@@ -6,6 +6,7 @@ const c = @cImport({
 
 /// An allocator based on [jemalloc](https://jemalloc.net/jemalloc.3.html)
 pub const allocator = std.mem.Allocator{
+    // `ptr` not used
     .ptr = undefined,
     .vtable = &.{
         .alloc = alloc,
@@ -19,13 +20,13 @@ pub const allocator = std.mem.Allocator{
 pub fn collectMallocStats(wtr: anytype, opts: ?[:0]const u8) void {
     const context = @constCast(&wtr);
     c.je_malloc_stats_print(struct {
-        fn f(ctx: ?*anyopaque, msg: [*c]const u8) callconv(.C) void {
+        fn cb(ctx: ?*anyopaque, msg: [*c]const u8) callconv(.C) void {
             const p: *@TypeOf(wtr) = @alignCast(@ptrCast(ctx));
             p.writeAll(std.mem.span(msg)) catch |e| {
-                std.log.err("call malloc_stats_print cb failed, err:{any}\n", .{e});
+                std.log.err("call malloc_stats_print cb failed, err:{any}", .{e});
             };
         }
-    }.f, context, if (opts) |v| v else null);
+    }.cb, context, if (opts) |v| v else null);
 }
 
 fn alloc(_: *anyopaque, n: usize, log2_align: u8, return_address: usize) ?[*]u8 {
@@ -58,7 +59,41 @@ test "basic alloc" {
     const ptr = try allocator.create(struct { a: i8, b: u64 });
     allocator.destroy(ptr);
 
-    // collectMallocStats(std.io.getStdErr().writer(), null);
+    var buf = std.ArrayList(u8).init(std.testing.allocator);
+    defer buf.deinit();
+    const wtr = buf.writer();
+    collectMallocStats(wtr, "J");
+
+    const Stats = struct {
+        jemalloc: struct {
+            version: []const u8,
+            config: struct {
+                debug: bool,
+                malloc_conf: []const u8,
+            },
+            opt: struct {
+                abort: bool,
+                abort_conf: bool,
+            },
+        },
+    };
+    const parsed = try std.json.parseFromSlice(Stats, allocator, buf.items, .{
+        .ignore_unknown_fields = true,
+    });
+
+    try std.testing.expectEqualDeep(parsed.value, Stats{
+        .jemalloc = .{
+            .version = "5.3.0",
+            .config = .{
+                .debug = true,
+                .malloc_conf = "",
+            },
+            .opt = .{
+                .abort = true,
+                .abort_conf = true,
+            },
+        },
+    });
 }
 
 test "alloc ArrayList" {
